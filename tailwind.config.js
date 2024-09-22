c7526f22ce141ffb569f6eae4ddab1b04358136f
istiakAHMEDsaad <istik2020@gmail.com>
2024-09-23 02:05:37 +0600

    Done style section

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      'lexend': ['Lexend', 'sans-serif'],
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dracula", "dark", "lofi"],
+  },
+}
