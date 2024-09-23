0eb55aa5e172b693b310e6bcc61ab70c5abd1563
istiakAHMEDsaad <istik2020@gmail.com>
2024-09-23 17:16:09 +0600

    header blur part

---
+++ JavaScript/main.js
@@ -0,0 +1,5 @@
+// ! Donation button content selector
+document.addEventListener('DOMContentLoaded', function(){
+    const donationButton = document.getElementById('donate-btn');
+    const historyButton = document.getElementById('');
+})
