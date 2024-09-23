b9465712b405c3786c18eeb859df0614b1c75003
istiakAHMEDsaad <istik2020@gmail.com>
2024-09-23 23:37:20 +0600

    add transaction features

--- JavaScript/main.js
+++
@@ -1,5 +0,0 @@
-// ! Donation button content selector
-document.addEventListener('DOMContentLoaded', function(){
-    const donationButton = document.getElementById('donate-btn');
-    const historyButton = document.getElementById('');
-})
